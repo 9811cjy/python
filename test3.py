e8f973526ee84f222f3335437d32ab3a8be6c291
9811cjy <54702551+9811cjy@users.noreply.github.com>
2019-08-30 16:40:49 +0900

    190830_py_basic_test
    Few initial workouts with python

---
+++ test3.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 30 14:43:28 2019
+
+@author: jyc5978
+"""
+
+print ('안녕')
+
+#%%
+print('안녕'+'하세요')
+
+#%%
+print('안녕','하세요')
+
+#%%
+print('1+4')
+
+#%%
+print(1+4)
+
+#%%
+print(1/4)
+
+#%%
+print('5'/3)
+
+#%%
+print('5'+'3')
+
+#%%
+print('5'*3)
+
+#%%
+a='5'
+print(a)
+b='고구마'
+milk='요구르트'
+_car = 153
+
+a=33
+print(a)
+
+class, if, while,def
+_class
+
+#%%
+1a=1
+print(1a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#%%
